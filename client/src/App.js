7eee4c57642745b4ea805031071bd041bb2e03be
Ken Orsini <korsini515@gmail.com>
2022-03-07 14:16:46 -0500

    alerts and auth components added

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,37 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 
+import AuthState from './context/auth/AuthState';
 import ContactState from './context/contact/ContactState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 const App = () => {
 	return (
-		<ContactState>
-			<Router>
-				<Fragment>
-					<Navbar />
-					<div className='container'>
-						<Routes>
-							<Route path='/' element={<Home />} />
-							<Route path='/about' element={<About />} />
-						</Routes>
-					</div>
-				</Fragment>
-			</Router>
-		</ContactState>
+		<AuthState>
+			<ContactState>
+				<AlertState>
+					<Router>
+						<Fragment>
+							<Navbar />
+							<div className='container'>
+								<Alerts />
+								<Routes>
+									<Route path='/' element={<Home />} />
+									<Route path='/about' element={<About />} />
+									<Route path='/register' element={<Register />} />
+									<Route path='/login' element={<Login />} />
+								</Routes>
+							</div>
+						</Fragment>
+					</Router>
+				</AlertState>
+			</ContactState>
+		</AuthState>
 	);
 };
 
